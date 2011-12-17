5e6c3e5561677e6f7e29644048015ddeef3e1545
Scott Sanbar <scott.sanbar@gmail.com>
2011-12-17 03:43:30 -0600

    Initial Commit

---
+++ src/RandomColorGradient.js
@@ -0,0 +1,181 @@
+function rCG_getRandomColor() {
+    var pre = "#000000"
+    var color = Math.floor(Math.random()
+        * 0x1000000).toString(16);
+    color = pre.substring(0,
+        pre.length - color.length) + color;
+
+    return color;
+}
+function rCG_initGlobals() {
+    G_RCG_DELTA_MAX = 50.0;
+    G_RCG_ORIGIN_X_MIN = 7;
+    gRCGAllowAnimation = true;
+    gRCGColorsHead = 0;
+    gRCGDelta = 0;
+    gRCGColors = new Array();
+    for (var i = 0; i < 10; i++ ) {
+        gRCGColors[i] = rCG_getRandomColor();
+    }
+    gRCGCanvas = document.
+        getElementById("idRCGCanvas");
+    gRCGStartStopUpdateCanvasButton = document.
+        getElementById("idRCGStartStopUpdateCanvas");
+    gRCGToggleGradientTypeButton = document.
+        getElementById("idRCGToggleGradientTypeButton");
+    gRCGContext = gRCGCanvas.getContext("2d");
+    gRCGCanvasWidth = gRCGCanvas.width;
+    gRCGCanvasHeight = gRCGCanvas.height;
+    gRCGOriginX = 0;
+    gRCGOriginY = gRCGCanvasHeight / 2.0;
+    gRCGMouseDown = false;
+}
+function rCG_updateCanvas() {
+    if ((typeof gRCGColors != "undefined") &&
+        (gRCGAllowAnimation)) {
+        gRCGDelta++;
+        if (gRCGDelta == G_RCG_DELTA_MAX) {
+            gRCGDelta = 0;
+            gRCGColorsHead--;
+            if (gRCGColorsHead < 0) {
+                gRCGColorsHead = gRCGColors.length - 1;
+            }
+            gRCGColors[gRCGColorsHead] =
+                rCG_getRandomColor();
+        }
+    }
+    try {
+        if (gRCGToggleGradientTypeButton.value == "Radial") {
+            var grd = gRCGContext.createLinearGradient
+                (0, 0, gRCGCanvasWidth, 0);
+        } else {
+            grd = gRCGContext.createRadialGradient
+                (gRCGOriginX, gRCGOriginY, 0, gRCGCanvasWidth,
+                 gRCGOriginY, gRCGCanvasWidth);
+        }        
+        var originXFrac = gRCGOriginX / gRCGCanvasWidth;
+        for (i = 0; i < gRCGColors.length; i++) {
+            if (i == 0) {
+            grd.addColorStop
+                (0, gRCGColors[(gRCGColorsHead + i)
+                        % gRCGColors.length]);
+            } else if (i == gRCGColors.length - 1) {
+                grd.addColorStop
+                    (1, gRCGColors[(gRCGColorsHead + i)
+                            % gRCGColors.length]);
+            } else {
+                var stopFrac = 1.0 / (gRCGColors.length - 2);
+                var stopDeltaFrac = ((i - 1) * stopFrac)
+                     + (gRCGDelta * stopFrac / G_RCG_DELTA_MAX);
+                if (gRCGToggleGradientTypeButton.value == "Radial") {
+//                    if (stopDeltaFrac < originXFrac) {
+                        stopDeltaFrac *= 1 - originXFrac;
+//                    } else {
+//                        stopDeltaFrac *= 2 * Math.abs(stopDeltaFrac - originXFrac) / 0.000000001;
+//                    }
+                    if (stopDeltaFrac < 0) {
+                        stopDeltaFrac = 0;
+                    }
+                }
+                grd.addColorStop
+                    (stopDeltaFrac, gRCGColors
+                     [(gRCGColorsHead + i)
+                             % gRCGColors.length]);
+            }
+        }
+        gRCGContext.fillStyle = grd;
+        gRCGContext.fillRect(0, 0, gRCGCanvasWidth, gRCGCanvasHeight);
+    } catch (err) {
+        alert("Error in updateCanvas(): " + err.message
+            + " Line Number: " + err.lineNumber);
+    }
+}
+function rCG_startStopUpdateCanvas() {
+    if (!isCanvasSupported()) {
+        alertNotSupported();
+        return;
+    }
+    if (typeof gRCGColors == "undefined") {
+        rCG_initGlobals();
+    }
+    try {
+        if (gRCGStartStopUpdateCanvasButton.value == "Start") {
+            gRCGIntervalID =
+                setInterval(rCG_updateCanvas, 20)
+            gRCGStartStopUpdateCanvasButton.value = "Stop";
+        } else {
+            clearInterval(gRCGIntervalID);
+            gRCGStartStopUpdateCanvasButton.value = "Start";
+        }
+    } catch (err) {
+        alert("Error in startStopUpdatecanvas(): "
+            + err.message
+            + " Line Number: " + err.lineNumber);
+    }
+}
+function rCG_toggleGradientType() {
+    if (!isCanvasSupported()) {
+        alertNotSupported();
+        return;
+    }
+    if (typeof gRCGColors == "undefined") {
+        rCG_initGlobals();
+    }
+    try {
+        if (gRCGToggleGradientTypeButton.value == "Linear") {
+            gRCGToggleGradientTypeButton.value = "Radial";
+        } else {
+            gRCGToggleGradientTypeButton.value = "Linear";
+        }
+        if (gRCGStartStopUpdateCanvasButton.value == "Start") {
+            gRCGAllowAnimation = false;
+            rCG_updateCanvas();
+            gRCGAllowAnimation = true;
+        }
+    } catch (err) {
+        alert("Error in toggleGradientType(): "
+            + err.message
+            + " Line Number: " + err.lineNumber);
+    }
+}
+function rCG_canvasMouseUp() {
+    gRCGMouseDown = false;
+}
+function rCG_canvasMouseOut() {
+    gRCGMouseDown = false;
+}
+
+function rCG_canvasMouseDown(evt) {
+    if (typeof gRCGColors == "undefined") {
+        rCG_initGlobals();
+    }
+    gRCGOriginX = getMousePos(gRCGCanvas, evt).x
+    if (gRCGOriginX < G_RCG_ORIGIN_X_MIN) {
+        gRCGOriginY = gRCGCanvasHeight / 2;
+        gRCGOriginX = 0;
+        gRCGMouseDown = false;
+    } else {
+        gRCGOriginY = getMousePos(gRCGCanvas, evt).y
+        gRCGMouseDown = true;
+    }
+    rCG_updateCanvas();
+        
+}
+
+function rCG_canvasMouseMove(evt) {
+    if (typeof gRCGColors == "undefined") {
+        rCG_initGlobals();
+    }
+    if (gRCGMouseDown == false) {
+      return;
+    }
+    gRCGOriginX = getMousePos(gRCGCanvas, evt).x
+    gRCGOriginY = getMousePos(gRCGCanvas, evt).y
+
+    if (gRCGOriginX < G_RCG_ORIGIN_X_MIN) {
+        gRCGOriginY = gRCGCanvasHeight / 2;
+        gRCGOriginX = 0;
+        gRCGMouseDown = false;
+    }
+    rCG_updateCanvas();
+}
